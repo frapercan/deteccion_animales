72e2b9b792a4b72df567624391697d31f5c55c0f
xaxi <frapercan1@alum.us.es>
2022-03-09 19:00:48 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+print('ERES LO MAS BONITO DE MI VIDA JE')
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
